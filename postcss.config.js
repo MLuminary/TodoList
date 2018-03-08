a8c0d4cc6424e95859067f9c0de67eef0703eedd
MLuminary <418037025@qq.com>
2018-03-08 18:06:45 +0800

    配置vue 及postcss

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+const autoprefixer = require('autoprefixer')
+
+//自动处理css属性加前缀
+module.exports = {
+  plugins:[
+    autoprefixer()
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ const config = {
         loader: 'vue-loader'
       },
       {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
@@ -34,6 +38,12 @@ const config = {
         use:[
           'style-loader',
           'css-loader',
+          {
+            loader:'postcss-loader',
+            options:{
+              sourceMap:true
+            }
+          },
           'stylus-loader'
         ]
       },
