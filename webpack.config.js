c25f974854fe03b21cbc170cc6e078ee3d0d6f45
MLuminary <418037025@qq.com>
2018-03-08 16:54:26 +0800

    webpack 配置css和image

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,46 @@
 const path = require('path')
 
 module.exports = {
+  //入口文件
   entry : path.join(__dirname, 'src/index.js'),
+  //出口文件
   output: {
     filename : 'bundle.js',
     path: path.join(__dirname, 'dist')
   },
+  //webpack 默认只能打包.js 其它后缀的文件需要自行定义
   module: {
     rules: [
       {
-        test: /.vue$/,
+        test: /\.vue$/,
         loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use:[
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader:'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name].[ext]'
+            }
+          }
+        ]
       }
     ]
   }
