af36271cc20936b061f5e0b857a3b0d5c3910cec
MLuminary <418037025@qq.com>
2018-03-08 17:43:07 +0800

    配置webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 //webpack 打包
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+//获取package.json 中定义的 env NODE.ENV的变量
+const isDev = process.env.NODE.ENV === 'development'
 
-module.exports = {
+const config = {
+  //编译目标
+  target: 'web',
   //入口文件
   entry : path.join(__dirname, 'src/index.js'),
   //出口文件
@@ -38,11 +44,39 @@ module.exports = {
             loader:'url-loader',
             options: {
               limit: 1024,
-              name: '[name].[ext]'
+              name: '[name]-aaa.[ext]'
             }
           }
         ]
       }
     ]
-  }
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        NODE_ENV : isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin()
+  ]
 }
+
+//如果是开发环境
+if (isDev){
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true,
+    },
+    //使页面局部刷新
+    hot: true
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+}
+
+module.exports = config;
